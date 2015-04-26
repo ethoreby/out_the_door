ac7e6c50dbfe809ec51845fbedae91b844b001c8
Emily Coco <Emily.Coco@audaxhealth.com>
2015-04-25 17:02:20 -0700

    add geolocation test for accuracy.

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -3,44 +3,86 @@ angular.module('starter.controllers', [])
 .controller('AppCtrl', function($scope, $ionicModal, $timeout, $cordovaGeolocation, $ionicPlatform) {
   // Form data for the login modal
   $scope.loginData = {};
+            $scope.setInitDistance = function() {
+                var posOptions = {timeout: 10000, enableHighAccuracy: true};
+                $cordovaGeolocation
+                    .getCurrentPosition(posOptions)
+                    .then(function (position) {
+                        var lat  = position.coords.latitude
+                        var long = position.coords.longitude
+                        $scope.oldLat = lat;
+                        $scope.oldLong = long;
+                        console.log('yo',lat, long)
+                    }, function(err) {
+                        console.log(err)
+                        // error
+                    });
+            };
 
-  $timeout(function() {
-
-  })
-//        var posOptions = {timeout: 10000, enableHighAccuracy: false};
-//            $cordovaGeolocation
-//                .getCurrentPosition(posOptions)
-//                .then(function (position) {
-//                    var lat  = position.coords.latitude
-//                    var long = position.coords.longitude
-//                    $scope.lat = lat;
-//                    $scope.long = long;
-//                    console.log('yo',lat, long)
-//                }, function(err) {
-//                    console.log(err)
-//                    // error
-//                });
-
-        var watchOptions = {
-            frequency : 100,
-            timeout : 300,
-            enableHighAccuracy: false // may cause errors if true
+        $scope.setNewDistance = function() {
+            var posOptions = {timeout: 10000, enableHighAccuracy: true};
+            $cordovaGeolocation
+                .getCurrentPosition(posOptions)
+                .then(function (position) {
+                    var lat  = position.coords.latitude
+                    var long = position.coords.longitude
+                    $scope.lat = lat;
+                    $scope.long = long;
+                    console.log('yo',lat, long)
+                }, function(err) {
+                    console.log(err)
+                    // error
+                });
         };
 
-        var watch = $cordovaGeolocation.watchPosition(watchOptions);
-        watch.then(
-            null,
-            function(err) {
-                console.log(err)
-                // error
-            },
-            function(position) {
-                var lat  = position.coords.latitude
-                var long = position.coords.longitude
-                $scope.lat = lat;
-                    $scope.long = long;
-                console.log(position)
-            });
+        $scope.getDistanceChange = function() {
+            $scope.distanceChange = getDistance($scope.oldLat, $scope.oldLong, $scope.lat, $scope.long);
+        };
+
+
+        var getDistance = function(lat1, lon1, lat2, lon2) {
+            var toRad = function(num) {
+                return num * Math.PI / 180;
+            }
+
+            var R = 6371; // km
+//has a problem with the .toRad() method below.
+            var x1 = lat2-lat1;
+            var dLat = toRad(x1);
+            var x2 = lon2-lon1;
+            var dLon = toRad(x2);
+            var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+                Math.cos(toRad(lat1)) * Math.cos(toRad(lat2)) *
+                Math.sin(dLon/2) * Math.sin(dLon/2);
+            var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+            var d = R * c;
+
+            return d/1.60934;
+        };
+
+
+
+        var watchOptions = {
+            frequency : 10000,
+            timeout : 30000,
+            enableHighAccuracy: true // may cause errors if true
+        };
+
+//        var watch = $cordovaGeolocation.watchPosition(watchOptions);
+//        watch.then(
+//            null,
+//            function(err) {
+//                console.log(err)
+//                // error
+//            },
+//            function(position) {
+//                var lat  = position.coords.latitude
+//                var long = position.coords.longitude
+//                $scope.lat = lat;
+//                $scope.long = long;
+//                $scope.distance = getDistance(37.33233141, -122.0312186, $scope.lat, $scope.long);
+//                console.log(lat, long)
+//            });
 
 
         // Create the login modal that we will use later
